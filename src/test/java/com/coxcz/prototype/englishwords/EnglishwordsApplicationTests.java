787625c7806e6d9a79bdfd52bf020e56b13e8203
Petr Fuks <pfuks@vendavo.com>
2020-04-15 09:40:50 +0200

    Initial commit

---
+++ src/test/java/com/coxcz/prototype/englishwords/EnglishwordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coxcz.prototype.englishwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishwordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
